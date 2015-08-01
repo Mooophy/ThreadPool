48de028fc8ba9fc67bfdcf077f00d324b54c5a55
yue <MoophyAlan@gmail.com>
2015-08-01 19:41:11 +1200

    polished in my style

--- ThreadPool.h
+++ ThreadPool.h
@@ -34,7 +34,7 @@ private:
 inline ThreadPool::ThreadPool(size_t threads)
     :   stop(false)
 {
-    for(size_t i = 0;i<threads;++i)
+    for(size_t i = 0; i < threads; ++i)
         workers.emplace_back(
             [this]
             {
@@ -43,9 +43,8 @@ inline ThreadPool::ThreadPool(size_t threads)
                     std::function<void()> task;
 
                     {
-                        std::unique_lock<std::mutex> lock(this->queue_mutex);
-                        this->condition.wait(lock,
-                            [this]{ return this->stop || !this->tasks.empty(); });
+                        std::unique_lock<std::mutex> lock( this->queue_mutex );
+                        this->condition.wait(lock, [this]{ return this->stop || !this->tasks.empty(); });
                         if(this->stop && this->tasks.empty())
                             return;
                         task = std::move(this->tasks.front());
